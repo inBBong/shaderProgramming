2fbd6e5d2951843d34690c4de719aa8de15efdc0
dsa <asd@sad>
2025-09-09 16:01:00 +0900

    triangle

--- SimpleGame/Renderer.cpp
+++ SimpleGame/Renderer.cpp
@@ -46,6 +46,18 @@ void Renderer::CreateVertexBufferObjects()
 	glGenBuffers(1, &m_VBORect);
 	glBindBuffer(GL_ARRAY_BUFFER, m_VBORect);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(rect), rect, GL_STATIC_DRAW);
+
+	float testPos[]
+		=
+	{
+		0,0,0,
+		1,0,0,
+		1,1,0
+	};
+
+	glGenBuffers(1, &m_VBOTestRect);
+	glBindBuffer(GL_ARRAY_BUFFER, m_VBOTestRect);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(testPos), testPos, GL_STATIC_DRAW);
 }
 
 void Renderer::AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType)
@@ -188,6 +200,29 @@ void Renderer::DrawSolidRect(float x, float y, float z, float size, float r, flo
 
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
+
+void Renderer::DrawTest()
+{
+	//Program select
+	glUseProgram(m_SolidRectShader);
+
+	glUniform4f(glGetUniformLocation(m_SolidRectShader, "u_Trans"), 
+		0,0,0,1);
+	glUniform4f(glGetUniformLocation(m_SolidRectShader, "u_Color"), 
+		1,1,1,1);
+
+	int attribPosition = glGetAttribLocation(m_SolidRectShader, "a_Position");
+	glEnableVertexAttribArray(attribPosition);
+	glBindBuffer(GL_ARRAY_BUFFER, m_VBOTestRect);
+	glVertexAttribPointer(attribPosition, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
+
+	glDrawArrays(GL_TRIANGLES, 0, 6);
+
+	glDisableVertexAttribArray(attribPosition);
+
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+
 
 void Renderer::GetGLPosition(float x, float y, float *newX, float *newY)
 {
